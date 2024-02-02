3053925135131ec3b26ae36dc5c437ea1f49f3a1
Khushal Mali <khushalmali8448@gmail.com>
2024-02-02 19:13:51 +0530

    added tailwind

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,15 +1,22 @@
 import Header from "./Header";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
+import Loader from "./Loader";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+  // console.log(navigation);
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
+
       <main>
-        {/* <h1>Khushal</h1> */}
         <Outlet />
       </main>
+
       <CartOverview />
     </div>
   );
