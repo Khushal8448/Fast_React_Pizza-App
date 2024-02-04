e314a1018671bfeacaaebbd1b8519f5236d8b6b6
Khushal Mali <khushalmali8448@gmail.com>
2024-02-04 11:35:06 +0530

    tailwind configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: 'Roboto Mono, monospace',
+    },
+    extend: {
+      colors: {
+        pizza: '#967395',
+      },
+      height: {
+        screen: '100dvh',
+      },
+    },
   },
   plugins: [],
 };
