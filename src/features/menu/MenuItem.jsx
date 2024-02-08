9d061293ca159ee0987a46bcfebc71a876abc363
Khushal Mali <khushalmali8448@gmail.com>
2024-02-08 20:45:49 +0530

    refactoring

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,13 +1,14 @@
-import Button from '../../ui/Button';
-import { formatCurrency } from '../../utils/helpers';
 import { useDispatch, useSelector } from 'react-redux';
-import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
+import Button from '../../ui/Button';
 import DeleteItem from '../cart/DeleteItem';
 import UpdateItemQuantity from '../cart/UpdateItemQuantity';
+import { formatCurrency } from '../../utils/helpers';
+import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
 
 function MenuItem({ pizza }) {
-  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
   const dispatch = useDispatch();
+
+  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   const currentQuantity = useSelector(getCurrentQuantityById(id));
   const isInCart = currentQuantity > 0;
@@ -39,10 +40,11 @@ function MenuItem({ pizza }) {
           {!soldOut ? (
             <p className="text-sm">{formatCurrency(unitPrice)}</p>
           ) : (
-            <p className="text-sm font-medium uppercase text-stone-500 line-through">
+            <p className="text-sm font-medium uppercase text-stone-500">
               Sold out
             </p>
           )}
+
           {isInCart && (
             <div className="flex items-center gap-3 sm:gap-8">
               <UpdateItemQuantity
